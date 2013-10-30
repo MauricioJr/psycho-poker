9028b55a33ceb3e6b260ea7df3cd949f29689048
Mauricio Junior Carvalho <maurcarvalho@gmail.com>
2013-10-30 16:38:47 -0200

    Criacao da Interface FileReader

--- src/br/com/amil/business/FileReader.java
+++ src/br/com/amil/business/FileReader.java
@@ -1,31 +1,9 @@
 package br.com.amil.business;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
-public class FileReader {
-	
-	private static final String resourceFile = "/src/main/resources/game.txt";
+public interface FileReader {
 
-	public FileInputStream getResourceFile(String filePath) throws FileNotFoundException{
-		String userDir = System.getProperty("user.dir");
-		return new FileInputStream(userDir + filePath);
-	}
-	
-	public List<String> getFileLine() throws IOException {
-		String logLineContent = new String();
-		List<String> resourceLines = new ArrayList<String>();
-		
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getResourceFile(resourceFile)));
-		
-		while ((logLineContent = bufferedReader.readLine()) != null) {
-			resourceLines.add(logLineContent);
-		}
-			return resourceLines;
-	}
+	public List<String> getFileLine() throws IOException;
 }
