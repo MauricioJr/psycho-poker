e2123ec00d25dfcdd81fd6fe7b692fa5f5b9a52c
Mauricio Junior Carvalho <maurcarvalho@gmail.com>
2013-10-30 16:48:11 -0200

    Enum de Jogadas

---
+++ src/br/com/amil/constants/PokerHand.java
@@ -0,0 +1,13 @@
+package br.com.amil.constants;
+
+public enum PokerHand{
+	straight_flush ,
+	four_of_a_kind ,
+	full_house ,
+	flush  ,
+	straight ,
+	three_of_a_kind,
+	two_pairs,
+	one_pair,
+	highest_card 
+}
